16ab4697381074fb62bc0520a52098a602bee9d9
FunSumTime <austin.espinoza98@gmail.com>
2026-02-12 11:21:15 -0700

    added a way to submit a image and the backend recieves it and says it recieves it. now just need add a stream to take a image and give it ot a model and return what the model gave.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def health():
     return "server is up"
 
+@app.route("/submit",methods=['GET','POST'])
+def get_submited_image():
+    if request.method == 'POST':
+        if 'my_image' not in request.files:
+            return 'no file part', 400
+    # Logic for handling POST request (e.g., process form submission)
+        file = request.files['my_image']
+        if file.filename == '':
+            return 'No slected file', 400
+        if file:
+            return f'File {file.filename} uploaded and recieved'
+        return 'Form submitted successfully!'
+    else:
+    # Logic for handling GET request (e.g., display the form)
+        return 'Please log in with a form.'
 
 if __name__ == '__main__':
     app.run()
+    
